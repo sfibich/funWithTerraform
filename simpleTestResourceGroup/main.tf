aa8c70b5f657b788c865835bca0cae35dc1b425b
sfibich <steve.fibich@gmail.com>
2021-01-11 12:02:33 -0500

    Update main.tf

--- simpleTestResourceGroup/main.tf
+++ simpleTestResourceGroup/main.tf
@@ -4,7 +4,7 @@ terraform {
     resource_group_name  = "terraform-mgmt-rg"
     storage_account_name = "a4xhqldweterraform"
     container_name       = "terraform-state"
-    key                  = "terraform.tfstate.sfibich.simpleTest"
+    key                  = "terraform.tfstate.simpleTest"
   }
   required_providers {
     azurerm = {
