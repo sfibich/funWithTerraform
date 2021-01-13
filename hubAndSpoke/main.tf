cb589bb55b425090e43f55621cdc7be88d9cdb53
sfibich <sfibich@example.com>
2021-01-13 13:06:09 +0000

    start of a simple hub and spoke network

---
+++ hubAndSpoke/main.tf
@@ -0,0 +1,54 @@
+# Configure the Azure provider
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-mgmt-rg"
+    storage_account_name = "a4xhqldweterraform"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate.hubAndSpoke"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "nhrg" {
+  name     = "network-hub"
+  location = "eastus2"
+}
+
+resource "azurerm_virtual_network" "hub" {
+  name                = "hub"
+  location            = azurerm_resource_group.nhrg.location
+  resource_group_name = azurerm_resource_group.nhrg.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["168.63.129.16", "23.253.163.53", "198.101.242.72"]
+
+}
+resource "azurerm_subnet" "gatewaySubnet" {
+  name                 = "GatewaySubnet"
+  address_prefix       = "10.0.255.0/27"
+  virtual_network_name = azurerm_virtual_network.hub.name
+  resource_group_name  = azurerm_resource_group.nhrg.name
+}
+
+resource "azurerm_subnet" "managementSubnet" {
+  name                 = "Management"
+  address_prefix       = "10.0.255.32/27"
+  virtual_network_name = azurerm_virtual_network.hub.name
+  resource_group_name  = azurerm_resource_group.nhrg.name
+}
+
+resource "azurerm_subnet" "azureBastionSubnet" {
+  name                 = "AzureBastionSubnet"
+  address_prefix       = "10.0.254.0/24"
+  virtual_network_name = azurerm_virtual_network.hub.name
+  resource_group_name  = azurerm_resource_group.nhrg.name
+}
