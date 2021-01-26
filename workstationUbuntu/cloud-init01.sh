328b0527ecffc3589f6df9db14aee0ee2ef79654
Ubuntu <adminuser@work-vm.wjlhjlqnypnurknlbx4agmquwc.cx.internal.cloudapp.net>
2021-01-26 09:05:34 -0500

    added azure func

--- workstationUbuntu/cloud-init01.sh
+++ workstationUbuntu/cloud-init01.sh
@@ -125,6 +125,15 @@ sudo apt-get update -y
 sudo apt-get install azure-cli -y 
 echo "complete:az cli" >> /tmp/cloud-init.log
 
+
+############################
+# Install  AZ Func         #
+############################
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install azure-functions-core-tools-3
+
 ############################
 # Work in Progress below   #
 ############################
